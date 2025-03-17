ef0fbbb07d2288fc5297fd75e2a76e4d5f9cb067
Dylan Swanson <daswanson22@gmail.com>
2025-03-16 17:00:29 -0700

    Customized the admin polls page

--- polls/models.py
+++ polls/models.py
@@ -2,16 +2,27 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         # Check if the published date is within the last day
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
+    @admin.display(\
+            boolean=True,
+            ordering="pub_date",
+            description="Published?",
+    )
     def is_published(self):
         now = timezone.now()
         return self.pub_date <= now
